366d4aafae18b954086a7ac6d407e87bc21e3c11
Neel Raja <neelrr1@gmail.com>
2020-12-20 15:52:15 -0800

    Create requirements txt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from canvasapi import Canvas
-#import dateutil.parser
 import ast
 import gcal
 import os.path
@@ -29,12 +28,9 @@ else:
 #print(d)
 if(True):
     for course in canvas.get_courses(enrollment_state='active'):
-        # print(course.name, course.id)
         if course.id == 130456:
             for assignment in course.get_assignments():
-                # print(assignment.name, dateutil.parser.isoparse(assignment.due_at))
                 if assignment.due_at is not None:
-                    #print(assignment.name)
                     if(assignment.id) in d.keys():
                         if((assignment.name,assignment.due_at) != d[assignment.id]):
                             print(gcal.editEvent(assignment.id, assignment.name + '*', assignment.due_at, calendarid))
