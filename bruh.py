45cf755192033b40bde9b7a9bbaac433fa9662b1
Santosh Lakshman <lathalaxsan@gmail.com>
2020-12-19 18:50:52 -0800

    You can find Neels's grades here

---
+++ bruh.py
@@ -0,0 +1,12 @@
+# Import the Canvas class
+from canvasapi import Canvas
+
+# Canvas API URL
+API_URL = "https://example.com"
+# Canvas API key
+API_KEY = "p@$$w0rd"
+
+# Initialize a new Canvas object
+canvas = Canvas(API_URL, API_KEY)
+
+print(API_URL)
