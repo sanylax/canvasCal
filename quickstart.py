25c4e56f9d11fb4dceaeb91788f29f516f828a6f
Santosh Lakshman <lathalaxsan@gmail.com>
2020-12-19 20:03:30 -0800

    Added Calendar

---
+++ quickstart.py
@@ -0,0 +1,90 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+
+def main(assignmentID, assignmentName, assignmentTime):
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+    
+    # calendar = {
+    #     'summary': 'Canvas Assignments 2.0',
+    #     'timeZone': 'America/Los_Angeles'
+    # }
+
+    # created_calendar = service.calendars().insert(body=calendar).execute()
+    # ourCalendarId = created_calendar['id']
+    # print(ourCalendarId) 
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+    event = {
+        'summary': assignmentName,
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': assignmentID,
+        'start': {
+            'dateTime': '2020-12-19T17:00:00Z',
+            'timeZone': 'UTC',
+        },
+        'end': {
+            'dateTime': '2020-12-19T18:00:00Z',
+            'timeZone': 'UTC',
+        },
+        'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'attendees': [
+            {'email': 'lpage@example.com'},
+            {'email': 'sbrin@example.com'},
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10},
+            ],
+        },
+    }
+    event = service.events().insert(calendarId='primary', body=event).execute()
+
+    #events.insert()
+
+main('test assignment', '456', 10)
