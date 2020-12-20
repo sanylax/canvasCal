4414ef70568e2ad919ac29880eae02ecb358e3e6
Neel Raja <neelrr1@gmail.com>
2020-12-19 18:51:28 -0800

    Expand gitinogre

---
+++ bruh.py
@@ -0,0 +1,10 @@
+# Import the Canvas class
+from canvasapi import Canvas
+
+# Canvas API URL
+API_URL = "https://gatech.instructure.com"
+# Canvas API key
+API_KEY = "p@$$w0rd"
+
+# Initialize a new Canvas object
+canvas = Canvas(API_URL, API_KEY)
